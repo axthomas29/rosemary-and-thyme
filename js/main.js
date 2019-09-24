8e6c82339f2f84363bcedfc8f96cb3a792fc196f
axthomas29 <axthomas29@gmail.com>
2019-09-24 14:38:17 -0400

    adding css to second carousel

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 /* Flickity */
-
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity(elem, {
     // options
     cellAlign: 'left',
     contain: true
-    resize: true
 });
